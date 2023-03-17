587cbc6b32e72839d7414576fd1c711b186da563
DmitryChe056 <dmitry.chetyrbok@mail.ru>
2023-03-17 17:16:12 +0300

    Create FirstClass.java

---
+++ FirstClass.java
@@ -0,0 +1,8 @@
+public class FirstClass {
+    public static void main(String[] args) {
+        String x = "Dima";
+        char a = 'D',s = 'i',d = 'm', f = 'a';
+        System.out.println(x);
+        System.out.print(a + "\n" + s + "\n" + d + "\n" + f );
+    }
+}
